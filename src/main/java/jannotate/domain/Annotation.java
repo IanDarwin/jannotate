1b51602896b22ab4e6f88dbe10d5317fff950d1f
IanDarwin <ian@darwinsys.com>
2013-07-24 17:36:11 -0400

    Build up the data class to a minimum level.

--- src/main/java/jannotate/domain/Annotation.java
+++ src/main/java/jannotate/domain/Annotation.java
@@ -1,5 +1,38 @@
 package jannotate.domain;
 
-public class Annotation {
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class Annotation {
+	@Id
+	int id;
+	String name;
+	String api;
+	String use;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getApi() {
+		return api;
+	}
+	public void setApi(String api) {
+		this.api = api;
+	}
+	public String getUse() {
+		return use;
+	}
+	public void setUse(String use) {
+		this.use = use;
+	}
 }
