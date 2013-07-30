dc816e7d9f638b2bf356810e2498ea98dbd161a7
IanDarwin <darwinian@darwinsys.com>
2013-07-29 22:35:15 -0400

    Relent: Put @ on annotation names.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-insert into annotation(id,api,name,usage) values(-1,'Lang', 'Override','Ensure method overrides superclass method');
-insert into annotation(id,api,name,usage) values(-2,'JSF', 'EJB',  'Request injection of EJB proxy into JSF bean');
-insert into annotation(id,api,name,usage) values(-3,'EJB', 'Stateless',  'Define class as Stateless Session Bean (SLSB)');
+insert into annotation(id,api,name,usage) values(-1,'Lang', '@Override','Ensure method overrides superclass method');
+insert into annotation(id,api,name,usage) values(-2,'JSF', '@EJB',  'Request injection of EJB proxy into JSF bean');
+insert into annotation(id,api,name,usage) values(-3,'EJB', '@Stateless',  'Define class as Stateless Session Bean (SLSB)');
